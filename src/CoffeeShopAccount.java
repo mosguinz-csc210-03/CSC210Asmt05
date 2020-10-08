c9f9cae8891c7eb8b55d9bc9aa219b3a6a75f957
mosguinz <mos.guinz@gmail.com>
2020-10-07 21:25:29 -0700

    Add menu option

--- src/CoffeeShopAccount.java
+++ src/CoffeeShopAccount.java
@@ -17,6 +17,29 @@ public class CoffeeShopAccount {
     private String pin;
 
     public static void main(String[] args) {
+        int action = promptAction();
+    }
 
+    /**
+     * Prompt the user for a menu option.
+     *
+     * @return Returns an `int` that corresponds to the menu option.
+     */
+    private static int promptAction() {
+        int action;
+        boolean prompted = false;
+        do {
+            if (prompted) System.out.println("Please enter a valid number");
+            prompted = true;
+            System.out.println("\nChoose a number from the following menu:");
+            System.out.println("1. Check balance");
+            System.out.println("2. Deposit money");
+            System.out.println("3. Spend money");
+            System.out.println("4. Change 4-digit PIN number");
+            System.out.println("5. Exit");
+            System.out.print(">>> ");
+            action = scan.nextInt();
+        } while (action < 1 || action > 5);
+        return action;
     }
 }
